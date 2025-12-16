6ed32fec3fd9106c518c942050666411e0851552
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-15 16:18:01 -0800

    Add Docusaurus documentation site

---
+++ content/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  'index': { display: 'hidden' },
+  'valid-configuration-guide': 'Valid - Configuration Guide',
+  'valid-getting-started': 'Valid - Getting Started',
+  'api-reference': {
+    title: 'API Reference',
+    type: 'page',
+    href: '/api-docs/valid-jolli-api-yaml'
+  }
+}
